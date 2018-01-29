bffbe607c0a699712f85aaced63cdc334814be02
alexclp <alexandru.clapa@gmail.com>
2018-01-29 17:51:08 +0000

    Getting touch positions on the floor plan

--- [AICIAC]Admin-App/FloorPlanViewController.swift
+++ [AICIAC]Admin-App/FloorPlanViewController.swift
@@ -21,4 +21,16 @@ class FloorPlanViewController: UIViewController {
 	func setFloorPlanImage() {
 		floorPlanImageView.image = UIImage(named: imageName)
 	}
+	
+	// Detects a touch event on the screen
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		let touch = touches.first!
+		let touchPoint = touch.location(in: self.view)
+		if let floorPlanImage = floorPlanImageView.image {
+			let x = (touchPoint.x - floorPlanImageView.bounds.minX) * floorPlanImage.size.width / floorPlanImageView.bounds.width
+			let y = (touchPoint.y - floorPlanImageView.bounds.minY) * floorPlanImage.size.height / floorPlanImageView.bounds.height
+			print("Locations are x: \(x) and \(y)")
+		}
+	}
 }
