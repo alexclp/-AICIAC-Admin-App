f3ea551ae78c03d8c7f1b839b9010c265e4cf59b
alexclp <alexandru.clapa@gmail.com>
2018-01-31 09:32:39 +0000

    Room list view controller added

---
+++ [AICIAC]Admin-App/RoomListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RoomListViewController.swift
+//  [AICIAC]Admin-App
+//
+//  Created by Alexandru Clapa on 31/01/2018.
+//  Copyright © 2018 Alexandru Clapa. All rights reserved.
+//
+
+import UIKit
+
+class RoomListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
